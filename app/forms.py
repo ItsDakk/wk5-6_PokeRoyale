80df531c27c11df722af67f0baa868f18e482bde
Dakodah Bradley <comatose9592@gmail.com>
2022-05-04 15:49:50 -0500

    day2 branch

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField
-from wtforms.validators import InputRequired
+from wtforms.validators import DataRequired
 
 class PokedexForm(FlaskForm):
-    pokemon_name = StringField('Pokemon Name', validators=[InputRequired()])
+    pokemon_name = StringField('Pokemon Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
