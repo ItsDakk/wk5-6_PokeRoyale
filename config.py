caa6490c62ab5937548c42c1338be020f4ac5f44
Dakodah Bradley <comatose9592@gmail.com>
2022-05-09 10:46:15 -0500

    wkend updates

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/dak/Documents/CodingTemple/week5/main_pokedex/wk5_dy3_pokedex /wk5_dy1_pokedex/app.db' 
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///Users/dak/Documents/CodingTemple/week5/main_pokedex/wk5_dy3_pokedex/ wk5_dy1_pokedex/app.db' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
